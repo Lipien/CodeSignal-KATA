2bf6a2a9dc38f80ca67b163d564bc86c628e9e56
Lipien <adam@pawtel.pl>
2019-10-02 12:45:17 +0200

    Gitignore file added.

---
+++ src/pl/pawtel/Main.java
@@ -0,0 +1,24 @@
+package pl.pawtel;
+
+public class Main {
+    static int findDuplicate(int[] a) {
+        int temp = 0;
+        int duplicate = -1;
+        for (int i = 0; i < a.length - 1; i++) {
+            for (int j = i + 1; j < a.length; j++) {
+                if (a[i] == a[j]) {
+                    duplicate = a[j];
+                    break;
+                }
+            }
+
+        }
+        System.out.println("The duplicate is: " + duplicate);
+        return duplicate;
+    }
+
+    public static void main(String[] args) {
+        int[] a = {1, 4, 2, 2, 8};
+        findDuplicate(a);
+    }
+}
