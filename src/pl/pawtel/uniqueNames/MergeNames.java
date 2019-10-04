4749d1b7fd2d3ccaba614366bc4edf1e2a376715
Lipien <adam@pawtel.pl>
2019-10-04 15:45:26 +0200

    TestDome: Alert Service.

--- src/pl/pawtel/uniqueNames/MergeNames.java
+++ src/pl/pawtel/uniqueNames/MergeNames.java
@@ -3,13 +3,14 @@ package pl.pawtel.uniqueNames;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.TreeSet;
 
 public class MergeNames {
 
     public static String[] uniqueNames(String[] names1, String[] names2) {
-        Set<String> mySet1 = new HashSet<String>(Arrays.asList(names1));
-        Set<String> mySet2 = new HashSet<String>(Arrays.asList(names2));
-        Set<String> union = new HashSet<String>(mySet1);
+        Set<String> mySet1 = new TreeSet<>(Arrays.asList(names1));
+        Set<String> mySet2 = new TreeSet<>(Arrays.asList(names2));
+        Set<String> union = new TreeSet<>(mySet1);
         union.addAll(mySet2);
         return union.toArray(new String[union.size()]);
     }
@@ -18,7 +19,7 @@ public class MergeNames {
         String[] names1 = new String[]{"Ava", "Emma", "Olivia"};
         String[] names2 = new String[]{"Olivia", "Sophia", "Emma"};
         String[] names = uniqueNames(names1, names2);
-        Arrays.sort(names);
+        //Arrays.sort(names);
         for (String n : names)
             System.out.print(" " + n); // should print Ava, Emma, Olivia, Sophia
     }
